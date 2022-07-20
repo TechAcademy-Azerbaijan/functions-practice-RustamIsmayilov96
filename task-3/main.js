c0622e9b642624f7af50a0b16ed0a0b5c99cf76f
RustamIsmayilov96 <106178348+RustamIsmayilov96@users.noreply.github.com>
2022-07-20 19:11:33 +0300

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -3,7 +3,17 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+    /* 
+    Istifadəçinin daxil etdiyi dəyişəni qəbul edirik. 
+    Giriş veriləni String type olur. Riyazi əməliyyatları tətbiq etmək üçün parseInt() metodundan istifadə edərək Number type-a çevrilməlidir.
+    */
+    var reqem = parseInt(result.input); // 9
+    let arr = []
+    function eded (array){
+        for (let i = array.length-1; i >=0; i--) {
+           arr.push(array[i]) 
+            
+        }console.log(arr);
+    }
+    eded([92846,96841,33582,25998])
 });
