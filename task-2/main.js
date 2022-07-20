2ee5e57d24e23c0a445b98a3781f31eb25193a80
RustamIsmayilov96 <106178348+RustamIsmayilov96@users.noreply.github.com>
2022-07-20 19:41:52 +0300

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -1,24 +1,14 @@
-const prompt = require('prompt');
-prompt.start();
-
-
-prompt.get('input', function (err, result) {
-    /* 
-    Istifadəçinin daxil etdiyi dəyişəni qəbul edirik. 
-    Giriş veriləni String type olur. Riyazi əməliyyatları tətbiq etmək üçün parseInt() metodundan istifadə edərək Number type-a çevrilməlidir.
-    */
-    var reqem = parseInt(result.input); // 9
-    let tanitma = true
-    function eded (reqem){
-         for (let i = 2; i < reqem; i++) {
-            if(reqem%i ==0){
-                console.log("NO");
-                tanitma = false
-                break;
-            } 
-        }
-        if(tanitma===true){
-            console.log("Yes");
-        }
+let tanitma = true
+function eded (reqem){
+     for (let i = 2; i < reqem; i++) {
+        if(reqem%i ==0){
+            console.log("NO");
+            tanitma = false
+            break;
+        } 
     }
-});
+    if(tanitma===true){
+        console.log("Yes");
+    }
+}
+eded(4)
